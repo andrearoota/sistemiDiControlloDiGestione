937da2a86b43b7072236c3df2f36c45a2df85807
Andrea Rota <andrea.rota.98@gmail.com>
2022-11-26 15:14:36 +0100

    Refactor of currency. Now get the exchange rate from database

--- website/controller/currency.py
+++ website/controller/currency.py
@@ -1,21 +1,19 @@
-from website.model.model import Cliente
+from website.model.model import Cliente, Valuta
+from website import db
 
-# Secondo me possiamo salvare nel DB le conversioni e prenderle da lì, per rendere
-# il codice più dinamico
+def currencyConversion (initialValue, clientCode, type):
+    '''
+    Returns the converted value with the exchange rate.
 
-def currencyConversion (prezzo, codiceCliente, tipo) :
-    subtotaleInEuro = prezzo
-    clien1 = Cliente.query.filter_by(codiceCliente=codiceCliente).first()
-    valClien = clien1.valutaCliente
-    #print(" CONVERSIONE VALUTA ----> " + codiceCliente + " codice valuta: " + str(valClien))
-    if valClien == 2:  # caso in cui il cliente paga in dollari
-        if tipo == "BUDGET":
-            subtotaleInEuro = subtotaleInEuro * 0.94868  # conversione dollaro/euro a budget
-        else:
-            subtotaleInEuro = subtotaleInEuro * 0.8338  # conversione dollaro/euro a consuntivo
-    elif valClien == 3:  # caso in cui il cliente paga in yen
-        if tipo == "BUDGET":
-            subtotaleInEuro = subtotaleInEuro * 0.0081300813  # conversione yen/euro a budget
-        else:
-            subtotaleInEuro = subtotaleInEuro * 0.00740685875  # conversione yen/euro a consuntivo
-    return subtotaleInEuro
+            Parameters:
+                    initialValue (float): Value to convert
+                    clientCode (string): id client
+                    type (string): Budget/Consuntivo
+
+            Returns:
+                    convertedValue (float)
+    '''
+
+    # Get exchange rate from database 
+    stmt = db.select(Valuta.tassoCambioMedio).select_from(Cliente).join(Valuta, Valuta.codValuta == Cliente.valutaCliente).where(Cliente.codiceCliente == clientCode).where(Valuta.budOCons == type.upper())
+    return initialValue / db.session.scalars(stmt).one()
